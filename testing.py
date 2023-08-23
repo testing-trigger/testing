a6fec8aa50bdbba3fc6058f260b8d5c7dd9ea928
testing-trigger <138791604+testing-trigger@users.noreply.github.com>
2023-08-23 10:25:46 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 import boto3
 session = boto3.Session(
-    aws_access_key_id = "AKIAZ4TSNGSGEZOHN6HV",
-    aws_secret_access_key = "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"
+    aws_access_key_id = "AKIAZ4TSNGSGHFV5CO4L",
+    aws_secret_access_key = "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"
 )
